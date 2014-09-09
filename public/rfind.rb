1060e5077cf4ac5cb7a3a2a871ffe94f91e12f65
mbourn <mbourn@mail.csuchico.edu>
2014-09-09 15:05:02 -0700

    Fixed formatting of output, added -i flag to grep command, removed debugging lines

--- public/rfind.rb
+++ public/rfind.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/ruby
-
-#######################
-# DEBUGING
-ctr=1
-
+###############################################################################
+#  Written  by: mbourn
+#  Written  on: 9/5/14
+#  Description: This is a script that searches the current directory, and all
+#  				subdirectories, for either filenames or text files containing
+# 				a specified string.  It then prints the results to the screen.
+#  				This is the long, readable version with verbose comments. 
+###############################################################################
 
 #  This line executes a bash command that lists all current and 
 ## subdirectories.  The result is saved in a variable
@@ -11,10 +14,7 @@ directory_string=`find .`
 
 #  This line splits the string from above into an array using the
 ## \n between each line as the split between elements
-array_of_paths=directory_string.split("\n")
-#puts array_of_paths
-array_of_paths.sort!
-#puts array_of_paths
+array_of_paths=directory_string.split("\n").sort!
 
 #  Print the first line
 puts "Files with names that matches <#{ARGV[0]}>"
@@ -23,23 +23,24 @@ array_of_paths.each do |path|
 		puts "  #{path}"
 	end
 end
-puts "**************************************************"
+puts "*" * 50
 puts "Files with content that matches <#{ARGV[0]}>"
 
 
 #  Loop through the array, test each element to see if it contains 
 ## a full path to a file, test that file to see if it has the proper
 ## file extension, grep it if it does, print results if any found.
+not_first_pass=0
 array_of_paths.each do |path|
 	if ( path=~/.*\.((rb)|(erb)|(js)|(css)|(html)|(yml)|(txt))$/ )
-		grep_result=`grep -n \'#{ARGV[0]}\' #{path}`
+		grep_result=`grep -ni \'#{ARGV[0]}\' #{path}`
 		if ( grep_result.size > 0)
+			not_first_pass==1 ? puts('-' * 50) : not_first_pass = 1 
 			puts path
 			grep_result_array=grep_result.split("\n")
 			grep_result_array.each do |found_word| 
 				puts "  #{found_word}"
 			end
-			puts "--------------------------------------------------"
 		end
 	end
 end
